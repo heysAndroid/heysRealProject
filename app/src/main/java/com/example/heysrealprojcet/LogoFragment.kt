0efa1692bc7d9897d776e282eab823c694e20077
there <there@example.com>
2022-02-04 16:09:22 +0900

    로고 페이지

---
+++ app/src/main/java/com/example/heysrealprojcet/LogoFragment.kt
@@ -0,0 +1,27 @@
+package com.example.heysrealprojcet
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.databinding.LogoFragmentBinding
+
+class LogoFragment : Fragment() {
+    private lateinit var binding: LogoFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = LogoFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_logoFragment_to_introFragment)
+        }, 1500)
+    }
+}
