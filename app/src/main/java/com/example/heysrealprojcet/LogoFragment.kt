56cb57a48f69d0368371e54f4ec9d24c844b3594
Jiwon <senta2006@naver.com>
2022-02-19 23:48:50 +0900

    로고 화면 코드 정렬

--- app/src/main/java/com/example/heysrealprojcet/LogoFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/LogoFragment.kt
@@ -5,26 +5,25 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.heysrealprojcet.databinding.LogoFragmentBinding
 
 class LogoFragment : Fragment() {
-    private lateinit var binding: LogoFragmentBinding
+   private lateinit var binding: LogoFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val mWindow = requireActivity().window
-        mWindow.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+      val mWindow = requireActivity().window
+      mWindow.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        binding = LogoFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+      binding = LogoFragmentBinding.inflate(inflater, container, false)
+      return binding.root
+   }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Handler().postDelayed({
-            findNavController().navigate(R.id.action_logoFragment_to_introFragment)
-        }, 1500)
-    }
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+      Handler().postDelayed({
+         findNavController().navigate(R.id.action_logoFragment_to_introFragment)
+      }, 1500)
+   }
 }
