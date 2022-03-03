3c842e5387979c5f5bb4abc4d13f5cc520d47412
Jiwon <senta2006@naver.com>
2022-03-03 17:23:32 +0900

    나이 유효성 검사 추가

--- app/src/main/java/com/example/heysrealprojcet/ui/join/age/JoinAgeViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/age/JoinAgeViewModel.kt
@@ -1,8 +1,20 @@
 package com.example.heysrealprojcet.ui.join.age
 
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.lifecycle.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class JoinAgeViewModel : ViewModel() {
-   val age = MutableStateFlow("")
+   val age = MutableLiveData<String>()
+
+   private val _isEnabled = MutableLiveData<Boolean>()
+   val isEnabled: LiveData<Boolean> = _isEnabled
+
+   init {
+      viewModelScope.launch { age.asFlow().collect { isCorrect() } }
+   }
+
+   private fun isCorrect() {
+      _isEnabled.value = !age.value.isNullOrBlank()
+   }
 }
