553a3b9c4c41e31667b29af928c6a8cc794f44e2
Jiwon <senta2006@naver.com>
2022-03-03 15:41:16 +0900

    비밀번호 토글 기능 추가

--- app/src/main/java/com/example/heysrealprojcet/ui/join/password/JoinPasswordViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/password/JoinPasswordViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.heysrealprojcet.ui.join.password
 
+import android.text.InputType
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,9 +12,12 @@ import kotlinx.coroutines.launch
 class JoinPasswordViewModel : ViewModel() {
    val password = MutableStateFlow("")
    val description = MutableStateFlow("아직 8자리가 아니에요")
+   private val isPasswordVisible = MutableLiveData(true)
+   val passwordInputType = MutableLiveData(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
 
    private val _isEnabled = MutableLiveData<Boolean>()
    val isEnabled: LiveData<Boolean> = _isEnabled
+
 
    init {
       viewModelScope.launch { password.collect { isCorrect() } }
@@ -26,5 +30,15 @@ class JoinPasswordViewModel : ViewModel() {
       }
    }
 
+   fun togglePasswordVisible() {
+      isPasswordVisible.value?.let {
+         isPasswordVisible.value = !it
+      }
 
+      if (isPasswordVisible.value == true) {
+         passwordInputType.value = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+      } else {
+         passwordInputType.value = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+      }
+   }
 }
