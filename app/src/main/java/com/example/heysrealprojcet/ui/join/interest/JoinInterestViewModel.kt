bf882bf75c10a4a4609c5d36a5562ba844da4b89
Jiwon <senta2006@naver.com>
2022-03-11 16:19:50 +0900

    관심 분야 1개 이상 선택해야 가입 버튼 활성화되도록 수정

--- app/src/main/java/com/example/heysrealprojcet/ui/join/interest/JoinInterestViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/interest/JoinInterestViewModel.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.heysrealprojcet.model.User
 import com.example.heysrealprojcet.repository.SignupRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,15 +18,40 @@ import javax.inject.Inject
 @HiltViewModel
 class JoinInterestViewModel @Inject constructor(
    private val signupRepository: SignupRepository) : ViewModel() {
+
+   /*
+   * 관심 분야
+    */
    private val totalMax = 3
    private var total = MutableStateFlow(0)
    val totalString = MutableStateFlow("${total.value}/3")
    private val interestList = mutableListOf<String>()
 
+   /*
+   * 네트워크 호출
+    */
    private val _isSuccess = MutableLiveData<Boolean>()
    val isSuccess: LiveData<Boolean> = _isSuccess
 
+   /*
+   * 시작하기 버튼
+    */
+   private val _isEnabled = MutableLiveData<Boolean>()
+   val isEnabled: LiveData<Boolean> = _isEnabled
+
    val accessToken = MutableLiveData<String>()
+
+   init {
+      viewModelScope.launch {
+         total.collect {
+            isSelected()
+         }
+      }
+   }
+
+   private fun isSelected() {
+      _isEnabled.value = total.value > 0
+   }
 
    fun onClickInterest(v: View) {
       val item = v.tag.toString()
