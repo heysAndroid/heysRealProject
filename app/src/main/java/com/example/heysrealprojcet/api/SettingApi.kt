6c797de607cb3968b89ea1534e1e8e2b25f90bc3
Jiwon <senta2006@naver.com>
2022-03-13 00:30:06 +0900

    import 수정

--- app/src/main/java/com/example/heysrealprojcet/api/SettingApi.kt
+++ app/src/main/java/com/example/heysrealprojcet/api/SettingApi.kt
@@ -1,9 +1,10 @@
 package com.example.heysrealprojcet.api
 
 import com.example.heysrealprojcet.model.LoginResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface SettingApi {
    @GET("setup/categories")
-   suspend fun getCategory(): retrofit2.Response<LoginResponse>
+   suspend fun getCategory(): Response<LoginResponse>
 }
--- app/src/main/java/com/example/heysrealprojcet/api/SignUpApi.kt
+++ app/src/main/java/com/example/heysrealprojcet/api/SignUpApi.kt
@@ -2,10 +2,17 @@ package com.example.heysrealprojcet.api
 
 import com.example.heysrealprojcet.model.LoginResponse
 import com.example.heysrealprojcet.model.User
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
+/*
+* 회원가입 api
+ */
+
 interface SignUpApi {
+   // suspend function : 실행되다가 중지되었다가, 다시 실행될 수 있음
+   // => blocking 없이 시간이 오래 걸리는 작업도 수행 가능함
    @POST("auth/signup")
-   suspend fun signup(@Body user: User): retrofit2.Response<LoginResponse>
+   suspend fun signup(@Body user: User): Response<LoginResponse>
 }
