b0aa848b2a1d939adb82df3762f95cdb562f8feb
Jiwon <senta2006@naver.com>
2022-03-13 00:30:22 +0900

    네트워크 timeout 수정

--- app/src/main/java/com/example/heysrealprojcet/di/NetworkModule.kt
+++ app/src/main/java/com/example/heysrealprojcet/di/NetworkModule.kt
@@ -13,6 +13,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
+/*
+ * 네트워크 설정 파일
+ */
+
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
@@ -23,9 +27,9 @@ object NetworkModule {
    fun provideHttpClient(): OkHttpClient {
       return OkHttpClient.Builder()
          .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-         .readTimeout(10, TimeUnit.SECONDS)
-         .connectTimeout(10, TimeUnit.SECONDS)
-         .writeTimeout(10, TimeUnit.SECONDS)
+         .readTimeout(15, TimeUnit.SECONDS)
+         .connectTimeout(15, TimeUnit.SECONDS)
+         .writeTimeout(15, TimeUnit.SECONDS)
          .build()
    }
 
