afafdf58e804e032d1ce9000e47f8d4270b3d038
Jiwon <senta2006@naver.com>
2022-03-13 00:30:56 +0900

    SettingRepository 수정

--- app/src/main/java/com/example/heysrealprojcet/repository/SettingRepository.kt
+++ app/src/main/java/com/example/heysrealprojcet/repository/SettingRepository.kt
@@ -1,9 +1,19 @@
 package com.example.heysrealprojcet.repository
 
 import com.example.heysrealprojcet.api.SettingApi
+import com.example.heysrealprojcet.model.LoginResponse
+import com.example.heysrealprojcet.model.network.NetworkResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class SettingRepository @Inject constructor(
-    private val settingApi: SettingApi) {
-    suspend fun getCategory() = settingApi.getCategory()
+   private val settingApi: SettingApi) : BaseApiResponse() {
+   suspend fun getCategory(): Flow<NetworkResult<LoginResponse>> {
+      return flow {
+         emit(safeApiCall { settingApi.getCategory() })
+      }.flowOn(Dispatchers.IO)
+   }
 }
--- app/src/main/java/com/example/heysrealprojcet/ui/join/phone/JoinPhoneViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/phone/JoinPhoneViewModel.kt
@@ -1,49 +1,29 @@
 package com.example.heysrealprojcet.ui.join.phone
 
-import android.util.Log
 import androidx.lifecycle.*
-import com.example.heysrealprojcet.repository.SettingRepository
 import com.example.heysrealprojcet.util.UserPreference
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
-class JoinPhoneViewModel @Inject constructor(
-    private val settingRepository: SettingRepository) : ViewModel() {
-    val phoneNumber = MutableLiveData("")
+class JoinPhoneViewModel : ViewModel() {
+   val phoneNumber = MutableLiveData("")
 
-    private val _isEnabled = MutableLiveData<Boolean>()
-    val isEnabled: LiveData<Boolean> = _isEnabled
+   private val _isEnabled = MutableLiveData<Boolean>()
+   val isEnabled: LiveData<Boolean> = _isEnabled
 
-    init {
-        viewModelScope.launch { getCategory() }
-        viewModelScope.launch {
-            phoneNumber.asFlow().collect {
-                if (phoneNumber.value?.contains('-') == true) {
-                    UserPreference.phoneNumber = phoneNumber.value?.replace("-", "").toString()
-                }
-                isElevenDigit()
+   init {
+      viewModelScope.launch {
+         phoneNumber.asFlow().collect {
+            if (phoneNumber.value?.contains('-') == true) {
+               UserPreference.phoneNumber = phoneNumber.value?.replace("-", "").toString()
             }
-        }
-    }
+            isElevenDigit()
+         }
+      }
+   }
 
-    private fun isElevenDigit() {
-        _isEnabled.value = phoneNumber.value?.length == 13
-    }
-
-    // api 호출 테스트용도
-    fun getCategory() {
-        CoroutineScope(Dispatchers.IO).launch {
-            settingRepository.getCategory()?.let { response ->
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        Log.w("====== test ======", it.toString())
-                    }
-                }
-            }
-        }
-    }
+   private fun isElevenDigit() {
+      _isEnabled.value = phoneNumber.value?.length == 13
+   }
 }
