cced9fa3d1e56b8a8f622e940912cd222fefa8dd
Jiwon <senta2006@naver.com>
2022-03-15 01:54:22 +0900

    유저 타입에 따라 navigation 시작점 다르게 설정

--- app/src/main/java/com/example/heysrealprojcet/ui/intro/IntroFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/intro/IntroFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.heysrealprojcet.databinding.IntroFragmentBinding
 import com.example.heysrealprojcet.ui.main.MainActivity
+import com.example.heysrealprojcet.util.UserPreference
 
 class IntroFragment : Fragment() {
    private lateinit var binding: IntroFragmentBinding
@@ -31,12 +32,7 @@ class IntroFragment : Fragment() {
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
-      imageList = mutableListOf(
-         "커리어 빌딩을 위한\n관심정보 여기 다-있다!",
-         "성장하는 청춘 스토리\n같이 만들어가요!",
-         "팀 빌딩부터 소통까지\n여기서 다-해결해요!"
-      )
-
+      setImageList()
       customPagerAdapter = CustomPagerAdapter()
       customPagerAdapter.submitList(imageList)
 
@@ -44,20 +40,29 @@ class IntroFragment : Fragment() {
       binding.viewpager.registerOnPageChangeCallback(pageChangeCallback)
       binding.indicator.setViewPager(binding.viewpager)
 
-      // 스킵 버튼 클릭시
-      binding.btnSkip.setOnClickListener { binding.viewpager.currentItem = 2 }
+      binding.skipButton.setOnClickListener { binding.viewpager.currentItem = 2 }
+      binding.userButton.setOnClickListener { goToMain(true) }
       // 다음 버튼 누를때마다 viewPager 페이지 바꾸기
       binding.nextButton.setOnClickListener {
          when (positon) {
             0 -> binding.viewpager.currentItem = 1
             1 -> binding.viewpager.currentItem = 2
-            2 -> goToNonmemberMain()
+            2 -> goToMain(false)
          }
       }
    }
 
-   private fun goToNonmemberMain() {
+   private fun setImageList() {
+      imageList = mutableListOf(
+         "커리어 빌딩을 위한\n관심정보 여기 다-있다!",
+         "성장하는 청춘 스토리\n같이 만들어가요!",
+         "팀 빌딩부터 소통까지\n여기서 다-해결해요!"
+      )
+   }
+
+   private fun goToMain(isExistingUser: Boolean) {
       val intent = Intent(requireContext(), MainActivity::class.java)
+      UserPreference.isExistingUser = isExistingUser
       startActivity(intent)
       requireActivity().finish()
    }
@@ -72,11 +77,11 @@ class IntroFragment : Fragment() {
          when (binding.viewpager.currentItem) {
             2 -> {
                binding.nextButton.text = "가입전에 둘러볼래요!"
-               binding.guestButton.visibility = View.VISIBLE
+               binding.userButton.visibility = View.VISIBLE
             }
             1 -> {
                binding.nextButton.text = "다음"
-               binding.guestButton.visibility = View.INVISIBLE
+               binding.userButton.visibility = View.INVISIBLE
             }
          }
       }
--- app/src/main/java/com/example/heysrealprojcet/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/main/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.heysrealprojcet.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.heysrealprojcet.R
+import com.example.heysrealprojcet.util.UserPreference
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +12,18 @@ class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.main_activity)
+      setNavigationGraph()
+   }
+
+   private fun setNavigationGraph() {
+      val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostMain) as NavHostFragment
+      val navController = navHostFragment.navController
+      val navGraph = navController.navInflater.inflate(R.navigation.main_navigation)
+      if (UserPreference.isExistingUser) {
+         navGraph.startDestination = R.id.joinPhoneFragment
+      } else {
+         navGraph.startDestination = R.id.mainFragment
+      }
+      navHostFragment.navController.graph = navGraph
    }
 }
