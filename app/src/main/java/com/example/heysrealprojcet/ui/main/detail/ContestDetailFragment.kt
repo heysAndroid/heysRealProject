9692ca0f03ca32cc33915cc03f418fe5aff47f3d
Jiwon <senta2006@naver.com>
2022-03-28 19:49:06 +0900

    공모전/대외활동 상세 페이지 생성

--- app/src/main/java/com/example/heysrealprojcet/ui/main/detail/ContestDetailFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/main/detail/ContestDetailFragment.kt
@@ -1,32 +1,31 @@
-package com.example.heysrealprojcet.ui.main.content.filter
+package com.example.heysrealprojcet.ui.main.detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.example.heysrealprojcet.databinding.FilterFragmentBinding
+import com.example.heysrealprojcet.R
+import com.example.heysrealprojcet.databinding.ContestDetailFragmentBinding
 
-class FilterFragment : Fragment() {
-   private lateinit var binding: FilterFragmentBinding
-   private val viewModel: FilterViewModel by viewModels()
+class ContestDetailFragment : Fragment() {
+   private lateinit var binding: ContestDetailFragmentBinding
 
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
    ): View? {
-      binding = FilterFragmentBinding.inflate(inflater, container, false)
-      binding.vm = viewModel
+      binding = ContestDetailFragmentBinding.inflate(inflater, container, false)
       return binding.root
    }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
+      binding.goToHeys.setOnClickListener { goToHeys() }
+   }
 
-      binding.btnApply.setOnClickListener {
-         findNavController().navigateUp()
-      }
+   private fun goToHeys() {
+      findNavController().navigate(R.id.action_contestDetailFragment_to_heysListFragment)
    }
 }
