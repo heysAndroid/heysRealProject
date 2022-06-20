3528906778b76e4b1c9788a396841ff0d3b4e4dc
Jiwon <senta2006@naver.com>
2022-06-20 01:42:08 -0400

    회원가입 이름 입력 화면 디자인 적용

--- app/src/main/java/com/example/heysrealprojcet/ui/join/name/JoinNameViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/name/JoinNameViewModel.kt
@@ -1,8 +1,8 @@
 package com.example.heysrealprojcet.ui.join.name
 
+import android.view.View
 import androidx.lifecycle.*
 import com.example.heysrealprojcet.util.UserPreference
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class JoinNameViewModel : ViewModel() {
@@ -11,16 +11,28 @@ class JoinNameViewModel : ViewModel() {
    private val _isEnabled = MutableLiveData<Boolean>()
    val isEnabled: LiveData<Boolean> = _isEnabled
 
+   private val _isDescriptionVisible = MutableLiveData(View.INVISIBLE)
+   val isDescriptionVisible: LiveData<Int> = _isDescriptionVisible
+
    init {
       viewModelScope.launch {
          name.asFlow().collect {
             UserPreference.name = it
             isCorrect()
+            isVisible()
          }
       }
    }
 
    private fun isCorrect() {
-      _isEnabled.value = !name.value.isNullOrBlank()
+      _isEnabled.value = !name.value.isNullOrBlank() && name.value?.length!! < 4
+   }
+
+   private fun isVisible() {
+      if (name.value?.length!! > 3) {
+         _isDescriptionVisible.value = View.VISIBLE
+      } else {
+         _isDescriptionVisible.value = View.INVISIBLE
+      }
    }
 }
