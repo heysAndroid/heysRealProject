919ade800f37eb559dfe73e9afd68ed025bf835a
Jiwon <senta2006@naver.com>
2022-06-20 01:23:14 -0400

    회원가입 비밀번호 입력 화면 디자인 수정

--- app/src/main/java/com/example/heysrealprojcet/ui/join/password/JoinPasswordViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/password/JoinPasswordViewModel.kt
@@ -7,12 +7,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.heysrealprojcet.util.UserPreference
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class JoinPasswordViewModel : ViewModel() {
    val password = MutableStateFlow("")
-   val description = MutableStateFlow("아직 8자리가 아니에요")
    private val isPasswordVisible = MutableLiveData(true)
    val passwordInputType = MutableLiveData(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
 
@@ -29,13 +27,7 @@ class JoinPasswordViewModel : ViewModel() {
    }
 
    private fun isCorrect() {
-      if (password.value?.length == 8) {
-         _isEnabled.value = true
-         description.value = "알맞은 비밀번호에요"
-      } else {
-         _isEnabled.value = false
-         description.value = "아직 8자리가 아니에요"
-      }
+      _isEnabled.value = password.value?.length == 8
    }
 
    fun togglePasswordVisible() {
