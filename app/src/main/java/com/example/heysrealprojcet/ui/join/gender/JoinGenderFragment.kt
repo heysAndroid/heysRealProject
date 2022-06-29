73349363d0be034d783ac229e0d879bcae057138
Jiwon <senta2006@naver.com>
2022-06-29 02:57:32 -0400

    나이 입력 화면 디자인 적용

--- app/src/main/java/com/example/heysrealprojcet/ui/join/gender/JoinGenderFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/gender/JoinGenderFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.JoinGenderFragmentBinding
 
 class JoinGenderFragment : Fragment() {
@@ -23,7 +25,7 @@ class JoinGenderFragment : Fragment() {
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.okButton.setOnClickListener {
-         //   findNavController().navigate(R.id.action_joinGenderFragment_to_joinAgeFragment)
+         findNavController().navigate(R.id.action_joinGenderFragment_to_joinAgeFragment)
       }
 
       viewModel.isMale.observe(viewLifecycleOwner) { isMale ->
