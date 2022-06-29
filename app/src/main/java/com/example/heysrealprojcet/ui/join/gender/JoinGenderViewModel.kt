f9b68c62ee09b681c83f47e9fd83c459909faa39
Jiwon <senta2006@naver.com>
2022-06-29 03:01:46 -0400

    성별 기본으로 남자 설정

--- app/src/main/java/com/example/heysrealprojcet/ui/join/gender/JoinGenderViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/join/gender/JoinGenderViewModel.kt
@@ -8,7 +8,7 @@ import com.example.heysrealprojcet.enums.Gender
 import com.example.heysrealprojcet.util.UserPreference
 
 class JoinGenderViewModel : ViewModel() {
-   private val _isMale = MutableLiveData<Boolean>()
+   private val _isMale = MutableLiveData(true)
    val isMale: LiveData<Boolean> = _isMale
 
    private val _isFemale = MutableLiveData<Boolean>()
@@ -18,21 +18,25 @@ class JoinGenderViewModel : ViewModel() {
    val isNonBinary: LiveData<Boolean> = _isNonBinary
 
    fun onClickGender(v: View) {
-      if (v.tag == "male") {
-         _isMale.value = true
-         _isFemale.value = false
-         _isNonBinary.value = false
-         UserPreference.gender = Gender.Male.gender
-      } else if (v.tag == "female") {
-         _isMale.value = false
-         _isFemale.value = true
-         _isNonBinary.value = false
-         UserPreference.gender = Gender.Female.gender
-      } else {
-         _isMale.value = false
-         _isFemale.value = false
-         _isNonBinary.value = true
-         UserPreference.gender = Gender.NonBinary.gender
+      when (v.tag) {
+         "male" -> {
+            _isMale.value = true
+            _isFemale.value = false
+            _isNonBinary.value = false
+            UserPreference.gender = Gender.Male.gender
+         }
+         "female" -> {
+            _isMale.value = false
+            _isFemale.value = true
+            _isNonBinary.value = false
+            UserPreference.gender = Gender.Female.gender
+         }
+         else -> {
+            _isMale.value = false
+            _isFemale.value = false
+            _isNonBinary.value = true
+            UserPreference.gender = Gender.NonBinary.gender
+         }
       }
    }
 }
