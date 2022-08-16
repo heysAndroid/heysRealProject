64f45434f725ff1f0ebe859e47c6eb35d0abc16c
wlsdn9297 <wlsdn9297@naver.com>
2022-08-16 12:24:45 +0900

    채널 생성 이름 버튼 활성화

--- app/src/main/java/com/example/heysrealprojcet/HeysChannelNameFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/HeysChannelNameFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.heysrealprojcet.databinding.HeysChannelNameFragmentBinding
 
 class HeysChannelNameFragment : Fragment() {
    private lateinit var binding : HeysChannelNameFragmentBinding
+   private val viewModel: HeysChannelNameViewModel by viewModels()
 
    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
       binding = HeysChannelNameFragmentBinding.inflate(inflater, container, false)
+      binding.vm = viewModel
       return binding.root
    }
 
