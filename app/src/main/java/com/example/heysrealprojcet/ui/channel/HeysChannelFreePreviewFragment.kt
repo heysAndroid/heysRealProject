e6387b2d2f79d100ba5a9df791b01dd5d16ee79a
Jiwon <senta2006@naver.com>
2022-09-13 00:28:46 +0200

    채널 생성 미리보기 화면에서 생성 완료 화면으로 넘어가도록 수정

--- app/src/main/java/com/example/heysrealprojcet/ui/channel/HeysChannelFreePreviewFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/HeysChannelFreePreviewFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.HeysChannelFreePreviewFragmentBinding
 import com.example.heysrealprojcet.ui.main.MainActivity
 
@@ -31,6 +33,10 @@ class HeysChannelFreePreviewFragment : Fragment() {
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
+      binding.btnMake.setOnClickListener { goToDetail() }
+   }
 
+   private fun goToDetail() {
+      findNavController().navigate(R.id.action_heysChannelFreePreviewFragment_to_heysChannelDetailFragment)
    }
 }
