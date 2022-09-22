9f16682a1a28e757b09eb7c720d69f824016808d
wlsdn9297 <wlsdn9297@naver.com>
2022-09-22 22:20:01 +0900

    채널 알림, 개인 채널 생성

---
+++ app/src/main/java/com/example/heysrealprojcet/model/NotificationList.kt
@@ -0,0 +1,8 @@
+package com.example.heysrealprojcet.model
+
+data class NotificationList(
+   val image: Int,
+   val title: String,
+   val content: String,
+   val time: String
+)
--- app/src/main/java/com/example/heysrealprojcet/ui/user/myChannel/MyChannelFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/myChannel/MyChannelFragment.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.MyChannelFragmentBinding
 
 class MyChannelFragment : Fragment() {
    private lateinit var binding: MyChannelFragmentBinding
 
-   override fun onCreateView(
-      inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-   ): View? {
+   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
       binding = MyChannelFragmentBinding.inflate(inflater, container, false)
       return binding.root
    }
@@ -20,5 +20,16 @@ class MyChannelFragment : Fragment() {
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
+
+      binding.bell.setOnClickListener { gotoNotification() }
+      binding.mail.setOnClickListener { gotoPrivateNotification() }
+   }
+
+   private fun gotoNotification() {
+      findNavController().navigate(R.id.action_myChannelFragment_to_myChannelNotificationFragment)
+   }
+
+   private fun gotoPrivateNotification() {
+      findNavController().navigate(R.id.action_myChannelFragment_to_myChannelPrivateNotificationFragment)
    }
 }
