6c1dea6f28190d06b9629988fb734417181f48fa
Jiwon <senta2006@naver.com>
2022-10-15 02:58:40 +0200

    설정 화면에서 바텀 네비게이션 숨김 처리

--- app/src/main/java/com/example/heysrealprojcet/ui/user/setting/SettingFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/setting/SettingFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.SettingFragmentBinding
+import com.example.heysrealprojcet.ui.main.MainActivity
 
 class SettingFragment : Fragment() {
    private lateinit var binding: SettingFragmentBinding
@@ -19,6 +20,24 @@ class SettingFragment : Fragment() {
       return binding.root
    }
 
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      val mainActivity = activity as MainActivity
+      mainActivity.hideBottomNavigation(true)
+   }
+
+   override fun onResume() {
+      super.onResume()
+      val mainActivity = activity as MainActivity
+      mainActivity.hideBottomNavigation(true)
+   }
+
+   override fun onDestroy() {
+      super.onDestroy()
+      val mainActivity = activity as MainActivity
+      mainActivity.hideBottomNavigation(false)
+   }
+   
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
