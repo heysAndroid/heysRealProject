01e3b1eb778a60b885257bd9decfdbcc07be1256
Jiwon <senta2006@naver.com>
2022-10-17 18:14:37 +0200

    채널 생성 이름 edittext 글자수 표시

--- app/src/main/java/com/example/heysrealprojcet/ui/channel/create/name/ChannelNameFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/create/name/ChannelNameFragment.kt
@@ -1,11 +1,9 @@
 package com.example.heysrealprojcet.ui.channel.create.name
 
 import android.os.Bundle
-import android.text.InputFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -38,22 +36,7 @@ class ChannelNameFragment : Fragment() {
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
-
-      binding.edtIntroduce.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
-
-      viewModel.isEnabled.observe(viewLifecycleOwner) {
-         if (it) {
-            binding.letterCount.text = "${binding.edtIntroduce.length()}"
-            binding.edtIntroduce.background = ContextCompat.getDrawable(requireContext(), R.drawable.bg_channel_edittext_focused)
-            binding.btnNext.background = ContextCompat.getDrawable(requireContext(), R.drawable.bg_f7bc26_radius_8)
-         } else {
-            binding.btnNext.background = ContextCompat.getDrawable(requireContext(), R.drawable.bg_e1_radius_8)
-         }
-      }
-
-      binding.btnNext.setOnClickListener {
-         goToInform()
-      }
+      binding.btnNext.setOnClickListener { goToInform() }
    }
 
    private fun goToInform() {
