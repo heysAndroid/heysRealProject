f83d00eca75f52bf91581ae58278a36b8c0e9a52
Jiwon <senta2006@naver.com>
2022-10-19 22:20:33 +0200

    채널 목적 다이얼로그 DialogFragment 로 리팩토링

--- app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialog.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialog.kt
@@ -1,8 +1,10 @@
 package com.example.heysrealprojcet.ui.channel.dialog.purpose
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +12,12 @@ import android.view.Window
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.example.heysrealprojcet.databinding.ChannelPurposeDialogBinding
+import com.example.heysrealprojcet.enums.ChannelPurpose
+import com.example.heysrealprojcet.util.ChannelPreference
 
 class ChannelPurposeDialog : DialogFragment() {
    private lateinit var binding: ChannelPurposeDialogBinding
    private val viewModel by viewModels<ChannelPurposeDialogViewModel>()
-
    private lateinit var listener: ChannelPurposeDialogOnClickListener
 
    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -23,26 +26,76 @@ class ChannelPurposeDialog : DialogFragment() {
 
       dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
       dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+      dialog?.setCancelable(false)
+
+      previousSelectedPurpose()
+      Log.w("selectedPrevious: ", ChannelPreference.channelPurpose)
+
+      viewModel.selectedPurpose.observe(viewLifecycleOwner) {
+         unselectAllButton()
+         when (it) {
+            ChannelPurpose.Capability.purpose -> selectCapabilityButton()
+            ChannelPurpose.Skill.purpose -> selectSkillButton()
+            ChannelPurpose.Experience.purpose -> selectExperienceButton()
+            else -> selectPortfolioButton()
+         }
+      }
+
+      binding.btnSave.setOnClickListener {
+         // 채널 정보 fragment 로 선택값 전달
+         listener.onClick(viewModel.selectedPurpose.value.toString())
+         dialog?.dismiss()
+      }
       binding.closeButton.setOnClickListener { dismiss() }
-      viewModel.selectedPurpose.observe(viewLifecycleOwner) {}
+
       return binding.root
    }
 
-   /*
-      fun show() {
-         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-         dialog.setContentView(binding.root)
-         dialog.setCancelable(false)
-         dialog.show()
+   private fun selectCapabilityButton() {
+      binding.btnCapability.isSelected = true
+      binding.btnCapability.setTypeface(null, Typeface.BOLD)
+   }
 
-         binding.btnSave.setOnClickListener {
-            listener.onClick(viewModel.btnText)
-            dialog.dismiss()
-         }
-         binding.closeButton.setOnClickListener { dialog.dismiss() }
+   private fun selectSkillButton() {
+      binding.btnSkill.isSelected = true
+      binding.btnSkill.setTypeface(null, Typeface.BOLD)
+   }
+
+   private fun selectExperienceButton() {
+      binding.btnExperience.isSelected = true
+      binding.btnExperience.setTypeface(null, Typeface.BOLD)
+   }
+
+   private fun selectPortfolioButton() {
+      binding.btnPortfolio.isSelected = true
+      binding.btnPortfolio.setTypeface(null, Typeface.BOLD)
+   }
+
+   private fun unselectAllButton() {
+      with(binding) {
+         btnCapability.isSelected = false
+         btnCapability.setTypeface(null, Typeface.NORMAL)
+
+         btnSkill.isSelected = false
+         btnSkill.setTypeface(null, Typeface.NORMAL)
+
+         btnExperience.isSelected = false
+         btnExperience.setTypeface(null, Typeface.NORMAL)
+
+         btnPortfolio.isSelected = false
+         btnPortfolio.setTypeface(null, Typeface.NORMAL)
       }
-   */
+   }
+
+   private fun previousSelectedPurpose() {
+      when (ChannelPreference.channelPurpose) {
+         ChannelPurpose.Skill.purpose -> binding.btnSkill.isSelected = true
+         ChannelPurpose.Capability.purpose -> binding.btnCapability.isSelected = true
+         ChannelPurpose.Experience.purpose -> binding.btnExperience.isSelected = true
+         ChannelPurpose.Portfolio.purpose -> binding.btnPortfolio.isSelected = true
+         else -> {}
+      }
+   }
 
    fun setOnOKClickListener(listener: (String) -> Unit) {
       this.listener = object : ChannelPurposeDialogOnClickListener {
--- app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialogViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialogViewModel.kt
@@ -1,59 +1,19 @@
 package com.example.heysrealprojcet.ui.channel.dialog.purpose
 
-import android.graphics.Typeface
 import android.view.View
 import android.widget.Button
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.heysrealprojcet.enums.ChannelPurpose
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.example.heysrealprojcet.util.ChannelPreference
 
 class ChannelPurposeDialogViewModel : ViewModel() {
-   var selectedView: View? = null
-
-   private val selectMax = 1
-
-   var selectedTotal = MutableStateFlow(1)
-   var btnText = ChannelPurpose.Skill.purpose
-
    private val _selectedPurpose = MutableLiveData<String>()
    val selectedPurpose: LiveData<String> = _selectedPurpose
 
-
-   fun onClickPurpose2(v: View) {
-      val button = v as Button
-      _selectedPurpose.value = button.text.toString()
-   }
-
    fun onClickPurpose(v: View) {
       val button = v as Button
-      btnText = button.text.toString()
-      selectedView?.isSelected = false
-
-      if (selectedTotal.value < selectMax) {
-         if (v.isSelected) {
-            v.isSelected = false
-            button.setTypeface(null, Typeface.NORMAL)
-            selectedTotal.value -= 1
-         } else {
-            selectedView = v
-            v.isSelected = true
-            button.setTypeface(null, Typeface.BOLD)
-            selectedTotal.value += 1
-         }
-      } else {
-         if (v.isSelected) {
-            v.isSelected = false
-            button.setTypeface(null, Typeface.NORMAL)
-            selectedTotal.value -= 1
-         } else {
-            selectedView?.isSelected = false
-            (selectedView as Button).setTypeface(null, Typeface.NORMAL)
-            v.isSelected = true
-            button.setTypeface(null, Typeface.BOLD)
-            selectedView = v
-         }
-      }
+      _selectedPurpose.value = button.text.toString()
+      ChannelPreference.channelPurpose = selectedPurpose.value.toString()
    }
 }
