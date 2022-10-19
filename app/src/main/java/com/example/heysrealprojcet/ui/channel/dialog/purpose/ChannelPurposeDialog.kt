d2f5566751b23efdad0819e950e8d9fcf82d1fb1
Jiwon <senta2006@naver.com>
2022-10-19 02:28:32 +0200

    채널 생성 다이얼로그 Dialogfragment 로 리팩토링

--- app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialog.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialog.kt
@@ -1,47 +1,48 @@
 package com.example.heysrealprojcet.ui.channel.dialog.purpose
 
-import android.app.Dialog
-import android.content.Context
 import android.graphics.Color
-import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.Window
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.example.heysrealprojcet.databinding.ChannelPurposeDialogBinding
-import com.example.heysrealprojcet.enums.ChannelPurpose
 
-class ChannelPurposeDialog(private val context: Context, private val viewModel: ChannelPurposeDialogViewModel) {
-   private var binding: ChannelPurposeDialogBinding = ChannelPurposeDialogBinding.inflate(LayoutInflater.from(context))
-   private val dialog = Dialog(context)
+class ChannelPurposeDialog : DialogFragment() {
+   private lateinit var binding: ChannelPurposeDialogBinding
+   private val viewModel by viewModels<ChannelPurposeDialogViewModel>()
+
    private lateinit var listener: ChannelPurposeDialogOnClickListener
 
-   init {
+   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+      binding = ChannelPurposeDialogBinding.inflate(inflater, container, false)
       binding.vm = viewModel
 
-      val button = when (viewModel.btnText) {
-         ChannelPurpose.Capability.purpose -> binding.btnCapability
-         ChannelPurpose.Skill.purpose -> binding.btnSkill
-         ChannelPurpose.Foundation.purpose -> binding.btnExperience
-         else -> binding.btnPortfolio
-      }
-      button.isSelected = true
-      button.setTypeface(null, Typeface.BOLD)
-      viewModel.selectedView = button
+      dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+      dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+      binding.closeButton.setOnClickListener { dismiss() }
+      viewModel.selectedPurpose.observe(viewLifecycleOwner) {}
+      return binding.root
    }
 
-   fun show() {
-      dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-      dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-      dialog.setContentView(binding.root)
-      dialog.setCancelable(false)
-      dialog.show()
+   /*
+      fun show() {
+         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+         dialog.setContentView(binding.root)
+         dialog.setCancelable(false)
+         dialog.show()
 
-      binding.btnSave.setOnClickListener {
-         listener.onClick(viewModel.btnText)
-         dialog.dismiss()
+         binding.btnSave.setOnClickListener {
+            listener.onClick(viewModel.btnText)
+            dialog.dismiss()
+         }
+         binding.closeButton.setOnClickListener { dialog.dismiss() }
       }
-      binding.closeButton.setOnClickListener { dialog.dismiss() }
-   }
+   */
 
    fun setOnOKClickListener(listener: (String) -> Unit) {
       this.listener = object : ChannelPurposeDialogOnClickListener {
--- app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialogViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/purpose/ChannelPurposeDialogViewModel.kt
@@ -3,14 +3,28 @@ package com.example.heysrealprojcet.ui.channel.dialog.purpose
 import android.graphics.Typeface
 import android.view.View
 import android.widget.Button
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.heysrealprojcet.enums.ChannelPurpose
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class ChannelPurposeDialogViewModel : ViewModel() {
    var selectedView: View? = null
+
    private val selectMax = 1
+
    var selectedTotal = MutableStateFlow(1)
-   var btnText = "역량강화"
+   var btnText = ChannelPurpose.Skill.purpose
+
+   private val _selectedPurpose = MutableLiveData<String>()
+   val selectedPurpose: LiveData<String> = _selectedPurpose
+
+
+   fun onClickPurpose2(v: View) {
+      val button = v as Button
+      _selectedPurpose.value = button.text.toString()
+   }
 
    fun onClickPurpose(v: View) {
       val button = v as Button
