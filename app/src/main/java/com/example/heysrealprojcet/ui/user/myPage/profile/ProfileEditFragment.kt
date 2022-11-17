810240ab956fc9d50d2f9b8fa8ccf176a4831fef
wlsdn9297 <wlsdn9297@naver.com>
2022-11-17 22:45:07 +0900

    프로필 수정 UI 및 기능 추가

--- app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/profile/ProfileEditFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/profile/ProfileEditFragment.kt
@@ -1,19 +1,25 @@
 package com.example.heysrealprojcet.ui.user.myPage.profile
 
 import android.os.Bundle
+import android.text.InputFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.ProfileEditFragmentBinding
 
 class ProfileEditFragment : Fragment() {
    private lateinit var binding: ProfileEditFragmentBinding
+   private val viewModel: ProfileEditViewModel by viewModels()
 
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
    ): View? {
       binding = ProfileEditFragmentBinding.inflate(inflater, container, false)
+      binding.vm = viewModel
       return binding.root
    }
 
@@ -21,5 +27,15 @@ class ProfileEditFragment : Fragment() {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
 
+      binding.introduceEditText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(35))
+      binding.nameEditText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
+      binding.jobEditText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
+      binding.abilityCount.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
+
+      binding.okButton.setOnClickListener { gotoMyPage() }
+   }
+
+   private fun gotoMyPage() {
+      findNavController().navigate(R.id.action_profileEditFragment_to_myPageFragment)
    }
 }
