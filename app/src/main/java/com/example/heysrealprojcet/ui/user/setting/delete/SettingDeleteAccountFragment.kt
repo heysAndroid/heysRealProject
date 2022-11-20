1e3f2d5cd41476c1dc67916c3fb69e8f3d602553
wlsdn9297 <wlsdn9297@naver.com>
2022-11-20 18:02:28 +0900

    계정 삭제 사유 입력 기능 추가

--- app/src/main/java/com/example/heysrealprojcet/ui/user/setting/delete/SettingDeleteAccountFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/setting/delete/SettingDeleteAccountFragment.kt
@@ -5,13 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.heysrealprojcet.databinding.SettingDeleteAccountFragmentBinding
 
 class SettingDeleteAccountFragment : Fragment() {
    private lateinit var binding : SettingDeleteAccountFragmentBinding
+   private val viewModel: SettingDeleteAccountViewModel by viewModels()
 
    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
       binding = SettingDeleteAccountFragmentBinding.inflate(inflater, container, false)
+      binding.vm = viewModel
       return binding.root
+   }
+
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+      binding.lifecycleOwner = this
+
+      viewModel.isSelected.observe(viewLifecycleOwner) { isSelected ->
+         if (isSelected) {
+            binding.edtWrite.visibility = View.VISIBLE
+            binding.edtWrite.requestFocus()
+         } else {
+            binding.edtWrite.visibility = View.GONE
+         }
+      }
+
+      viewModel.isEnabled.observe(viewLifecycleOwner) { isEnabled ->
+         binding.btnNext.isEnabled = isEnabled
+      }
    }
 }
