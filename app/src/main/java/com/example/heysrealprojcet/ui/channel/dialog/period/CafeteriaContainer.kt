7e87d932dbf08bc5eb35cd2822d9e1e0b019e7f3
wlsdn9297 <wlsdn9297@naver.com>
2023-01-03 12:26:57 +0900

    날짜 선택 및 초기화 추가

--- app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/period/CafeteriaContainer.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/period/CafeteriaContainer.kt
@@ -1,6 +1,8 @@
 package com.example.heysrealprojcet.ui.channel.dialog.period
 
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.ItemCalendarDayBinding
 import com.kizitonwose.calendarview.CalendarView
 import com.kizitonwose.calendarview.model.CalendarDay
@@ -18,7 +20,20 @@ class CafeteriaContainer(
 
     init {
         view.setOnClickListener {
-
+            when (viewModel.selectedList.size) {
+                in 0..1 -> {
+                    viewModel.selectedList.add(day.date)
+                    viewModel.updateSelectedDate(viewModel.selectedList)
+                    calendarView.notifyDateChanged(day.date)
+                }
+                2 -> {
+                    viewModel.updateSelectedDate(arrayListOf(day.date, null))
+                    calendarView.notifyDateChanged(viewModel.selectedList[0]!!)
+                    calendarView.notifyDateChanged(viewModel.selectedList[1]!!)
+                    calendarView.notifyDateChanged(day.date)
+                    viewModel.selectedList = arrayListOf(day.date)
+                }
+            }
         }
     }
 
@@ -28,5 +43,36 @@ class CafeteriaContainer(
         if(day.owner == DayOwner.THIS_MONTH) {
             bind.itemCalendarDate.text = day.date.dayOfMonth.toString()
         }
+
+        bind.itemCalendarDate.setBackgroundResource(
+            when (day.date) {
+               viewModel.selectedDate.value?.get(0) -> {
+                   R.drawable.bg_calendar_select_circle
+               }
+               viewModel.selectedDate.value?.get(1) -> {
+                   R.drawable.bg_calendar_select_circle
+               }
+               else -> {
+                   0
+               }
+            }
+        )
+
+        bind.itemCalendarDate.setTextColor(
+            ContextCompat.getColor(
+                view.context,
+                when (day.date) {
+                    viewModel.selectedDate.value?.get(0) -> {
+                        R.color.white
+                    }
+                    viewModel.selectedDate.value?.get(1) -> {
+                        R.color.white
+                    }
+                    else -> {
+                        R.color.color_828282
+                    }
+                }
+            )
+        )
     }
 }
--- app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/period/ChannelPeriodDialogViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/dialog/period/ChannelPeriodDialogViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.heysrealprojcet.ui.channel.dialog.period
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
 
 class ChannelPeriodDialogViewModel : ViewModel() {
+   var selectedList = arrayListOf<LocalDate?>()
 
+   private val _selectedDate: MutableLiveData<ArrayList<LocalDate?>> = MutableLiveData()
+   val selectedDate: LiveData<ArrayList<LocalDate?>> = _selectedDate
+
+   fun updateSelectedDate(selectedDate: ArrayList<LocalDate?>) {
+      _selectedDate.value = selectedDate
+   }
 }
