5a80fa4285eecfb5c5d252fd1630a5466f656037
Jiwon <senta2006@naver.com>
2023-02-28 01:30:03 +0900

    프로필 수정 페이지에 mbti 선택란 추가

---
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/profile/MbtiViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.heysrealprojcet.ui.user.myPage.profile
+
+import android.graphics.Typeface
+import android.view.View
+import android.widget.Button
+import androidx.lifecycle.ViewModel
+import com.example.heysrealprojcet.util.UserPreference
+
+class MbtiViewModel : ViewModel() {
+   fun onClickMbti(v: View) {
+      var button = v as Button
+      button.isSelected = true
+      button.setTypeface(null, Typeface.BOLD)
+      UserPreference.mbti = button.text.toString()
+   }
+}
--- app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/profile/ProfileEditFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/profile/ProfileEditFragment.kt
@@ -1,41 +1,62 @@
 package com.example.heysrealprojcet.ui.user.myPage.profile
 
 import android.os.Bundle
-import android.text.InputFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.InterestViewModel
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.ProfileEditFragmentBinding
+import com.example.heysrealprojcet.util.UserPreference
 
 class ProfileEditFragment : Fragment() {
    private lateinit var binding: ProfileEditFragmentBinding
    private val viewModel: ProfileEditViewModel by viewModels()
+   private val mbtiViewModel: MbtiViewModel by viewModels()
+   private val interestViewModel: InterestViewModel by viewModels()
 
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
    ): View? {
       binding = ProfileEditFragmentBinding.inflate(inflater, container, false)
       binding.vm = viewModel
+      binding.vmMbti = mbtiViewModel
+      binding.vmInterest = interestViewModel
       return binding.root
    }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
-
-      binding.introduceEditText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(35))
-      binding.nameEditText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
-      binding.jobEditText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
-      binding.abilityCount.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10))
-
+      setMbti()
       binding.okButton.setOnClickListener { gotoMyPage() }
    }
 
    private fun gotoMyPage() {
       findNavController().navigate(R.id.action_profileEditFragment_to_myPageFragment)
+   }
+
+   private fun setMbti() {
+      when (UserPreference.mbti) {
+         "ISTJ" -> binding.mbtiView.istj.isSelected = true
+         "ISFJ" -> binding.mbtiView.isfj.isSelected = true
+         "INFJ" -> binding.mbtiView.infj.isSelected = true
+         "INTJ" -> binding.mbtiView.intj.isSelected = true
+         "ISTP" -> binding.mbtiView.istp.isSelected = true
+         "ISFP" -> binding.mbtiView.isfp.isSelected = true
+         "INFP" -> binding.mbtiView.infp.isSelected = true
+         "INTP" -> binding.mbtiView.intp.isSelected = true
+         "ESTP" -> binding.mbtiView.estp.isSelected = true
+         "ESFP" -> binding.mbtiView.esfp.isSelected = true
+         "ENFP" -> binding.mbtiView.enfp.isSelected = true
+         "ENTP" -> binding.mbtiView.entp.isSelected = true
+         "ESTJ" -> binding.mbtiView.estj.isSelected = true
+         "ESFJ" -> binding.mbtiView.esfj.isSelected = true
+         "ENFJ" -> binding.mbtiView.enfj.isSelected = true
+         "ENTJ" -> binding.mbtiView.entj.isSelected = true
+      }
    }
 }
