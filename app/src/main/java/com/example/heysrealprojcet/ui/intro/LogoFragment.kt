d855158eea0ac14bc66d024a7d1ffeb605b1c4ec
wlsdn9297 <wlsdn9297@naver.com>
2023-03-14 13:59:44 +0900

    브랜치 충돌 문제 해결

--- app/src/main/java/com/example/heysrealprojcet/ui/intro/LogoFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/intro/LogoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.heysrealprojcet.ui.intro
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -9,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.LogoFragmentBinding
+import com.example.heysrealprojcet.ui.main.MainActivity
+import com.example.heysrealprojcet.util.UserPreference
 
 class LogoFragment : Fragment() {
    private lateinit var binding: LogoFragmentBinding
@@ -30,9 +33,7 @@ class LogoFragment : Fragment() {
          )
       }
 
-      Handler(Looper.getMainLooper()).postDelayed({
-                                                     findNavController().navigate(R.id.action_logoFragment_to_introFragment)
-                                                  }, 3000)
+      Handler(Looper.getMainLooper()).postDelayed({ moveToNext() }, 3000)
    }
 
    override fun onDestroyView() {
@@ -42,6 +43,19 @@ class LogoFragment : Fragment() {
          activity?.window?.insetsController?.show(WindowInsets.Type.statusBars())
       } else {
          activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      }
+   }
+
+   private fun moveToNext() {
+      if (UserPreference.isAutoLogin) {
+         val intent = Intent(requireContext(), MainActivity::class.java).apply {
+            putExtra(Intent.EXTRA_TEXT, "mainHome")
+            type = "text/plain"
+         }
+         startActivity(intent)
+         requireActivity().finish()
+      } else {
+         findNavController().navigate(R.id.action_logoFragment_to_introFragment)
       }
    }
 }
