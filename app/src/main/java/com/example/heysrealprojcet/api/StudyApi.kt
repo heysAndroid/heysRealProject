047335a7832fb50076261b130e9899c11df0fe32
Jiwon <senta2006@naver.com>
2023-03-23 22:29:09 +0900

    메인에서 채널 리스트로 이동

--- app/src/main/java/com/example/heysrealprojcet/api/StudyApi.kt
+++ app/src/main/java/com/example/heysrealprojcet/api/StudyApi.kt
@@ -2,12 +2,24 @@ package com.example.heysrealprojcet.api
 
 import com.example.heysrealprojcet.model.Study
 import com.example.heysrealprojcet.model.network.response.CreateStudyResponse
+import com.example.heysrealprojcet.model.network.response.StudyListResponse
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface StudyApi {
    @POST("channel/study")
    suspend fun createStudy(@Header("Authorization") token: String, @Body study: Study): Response<CreateStudyResponse>
+
+   @GET("channel/study")
+   suspend fun getStudyList(
+      @Header("Authorization") token: String,
+      @Query("interests") interests: ArrayList<String>,
+      @Query("lastRecruitDate") lastRecruitDate: String,
+      @Query("purposes") purposes: ArrayList<String>,
+      @Query("online") online: String?,
+      @Query("location") location: String?,
+      @Query("includeClosed") includeClosed: Boolean?,
+      @Query("page") page: Int?,
+      @Query("limit") limit: Int?,
+   ): Response<StudyListResponse>
 }
