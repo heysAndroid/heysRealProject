80fd3f21c81b50356bc5ed802e811ceb510605ea
Jiwon <senta2006@naver.com>
2023-04-03 14:30:30 +0900

    마이페이지 프로필 이미지, mbti 설정

--- app/src/main/java/com/example/heysrealprojcet/model/MyPage.kt
+++ app/src/main/java/com/example/heysrealprojcet/model/MyPage.kt
@@ -1,6 +1,5 @@
 package com.example.heysrealprojcet.model
 
-import com.example.heysrealprojcet.enums.Gender
 import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -9,13 +8,15 @@ import kotlinx.serialization.Serializable
 data class MyPage(
    @SerializedName("userName") val name: String,
    @SerialName("phone") val phone: String,
-   @SerialName("gender") val gender: Gender,
-   @SerialName("age") val age: Int,
+   @SerialName("gender") val gender: String,
+   @SerialName("birthDate") val birthDate: String,
    @SerialName("job") val job: String,
-   @SerialName("profileUrl") val profileUrl: String,
+   @SerialName("profileLinks") val profileLinks: Array<String>,
    @SerialName("introduce") val introduce: String,
    @SerialName("capability") val capability: String,
+   @SerialName("userPersonality") val userPersonality: String,
    @SerialName("interests") val interests: Array<String>,
+   @SerialName("percentage") val percentage: Int,
    @SerialName("joinChannelCount") val joinChannelCount: Int,
    @SerialName("waitingChannelCount") val waitingChannelCount: Int,
 ) : java.io.Serializable
--- app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/MyPageFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/myPage/MyPageFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.MyPageFragmentBinding
+import com.example.heysrealprojcet.enums.Gender
 import com.example.heysrealprojcet.model.MyPage
 import com.example.heysrealprojcet.model.network.NetworkResult
 import com.example.heysrealprojcet.util.UserPreference
@@ -79,6 +80,7 @@ class MyPageFragment : Fragment() {
    }
 
    private fun setMyPageInfo(myPage: MyPage) {
+      // 자기소개
       if (myPage.introduce.isNullOrBlank()) {
          binding.introduce.text = "아직 소개할 내용이 없어요."
          binding.introduce.setTextColor(resources.getColor(R.color.color_828282))
@@ -87,14 +89,26 @@ class MyPageFragment : Fragment() {
          binding.introduce.setTextColor(resources.getColor(R.color.color_262626))
       }
 
+      // 이름
       binding.name.text = myPage.name
 
+      // mbti
+      if (myPage.userPersonality.isNullOrBlank()) {
+         binding.mbti.text = "????"
+         binding.mbti.isEnabled = false
+      } else {
+         binding.mbti.text = myPage.userPersonality
+         binding.mbti.isEnabled = true
+      }
+
+      // 관심분야/직무
       var interestString = ""
       myPage.interests.forEach {
          interestString += "#$it "
       }
       binding.interestContent.text = interestString
 
+      // 직업
       if (myPage.job.isNullOrBlank()) {
          binding.jobTitle.setTextColor(resources.getColor(R.color.color_4d828282))
          binding.job.text = "아직 소개할 직업이 없어요."
@@ -105,6 +119,7 @@ class MyPageFragment : Fragment() {
          binding.job.setTextColor(resources.getColor(R.color.color_262626))
       }
 
+      // 사용가능한 스킬
       if (myPage.capability.isNullOrBlank()) {
          binding.skillTitle.setTextColor(resources.getColor(R.color.color_4d828282))
          binding.skill.text = "아직 소개할 스킬이 없어요."
@@ -118,7 +133,42 @@ class MyPageFragment : Fragment() {
          binding.skill.text = skillString
          binding.skill.setTextColor(resources.getColor(R.color.color_262626))
       }
+      setByPercent(myPage.percentage, myPage.gender)
+
+      // 참여중인 채널 수
       binding.joinChannel.text = myPage.joinChannelCount.toString()
+      // 승인대기 채널 수
       binding.waitingChannel.text = myPage.waitingChannelCount.toString()
+   }
+
+   private fun setByPercent(percentage: Int, gender: String) {
+      // 프로필 이미지 설정
+      when (percentage) {
+         in 0..49 -> {
+            binding.editText.text = "프로필이 채워지지 않았어요"
+            when (gender) {
+               Gender.Male.genderEnglish -> binding.profileImage.setImageResource(R.drawable.ic_male_0)
+               Gender.Female.genderEnglish -> binding.profileImage.setImageResource(R.drawable.ic_female_0)
+               else -> binding.profileImage.setImageResource(R.drawable.ic_none_0)
+            }
+         }
+
+         in 50..99 -> {
+            binding.editText.text = "프로필을 더 채워주세요."
+            when (gender) {
+               Gender.Male.genderEnglish -> binding.profileImage.setImageResource(R.drawable.ic_male_50)
+               Gender.Female.genderEnglish -> binding.profileImage.setImageResource(R.drawable.ic_female_50)
+               else -> binding.profileImage.setImageResource(R.drawable.ic_none_50)
+            }
+         }
+         100 -> {
+            binding.editText.text = "완성된 프로필 카드를 만들었어요."
+            when (gender) {
+               Gender.Male.genderEnglish -> binding.profileImage.setImageResource(R.drawable.ic_male_100)
+               Gender.Female.genderEnglish -> binding.profileImage.setImageResource(R.drawable.ic_female_100)
+               else -> binding.profileImage.setImageResource(R.drawable.ic_none_100)
+            }
+         }
+      }
    }
 }
