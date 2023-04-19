ade94c162b205d2eccdb8c9f8c146d0a0efa5bbf
Jiwon <senta2006@naver.com>
2023-04-19 14:09:19 +0900

    enum 클래스 대외활동 Activity -> Extracurricular 로 변경

--- app/src/main/java/com/example/heysrealprojcet/enums/ChannelType.kt
+++ app/src/main/java/com/example/heysrealprojcet/enums/ChannelType.kt
@@ -1,7 +1,7 @@
 package com.example.heysrealprojcet.enums
 
-enum class ChannelType(val type: String) {
-   Contest("공모전"),
-   Activity("대외활동"),
-   Study("스터디")
+enum class ChannelType(val type: String, val typeEng: String) {
+   Contest("공모전", "Contest"),
+   Extracurricular("대외활동", "Extracurricular"),
+   Study("스터디", "Study")
 }
