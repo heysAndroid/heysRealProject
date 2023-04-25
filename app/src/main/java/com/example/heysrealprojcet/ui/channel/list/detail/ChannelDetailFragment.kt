ec1b5f2bf786172e2bc642158879ae1acd36a6cb
Jiwon <senta2006@naver.com>
2023-04-25 16:31:34 +0900

    채널 디테일 화면 수정

--- app/src/main/java/com/example/heysrealprojcet/ui/channel/list/detail/ChannelDetailFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/list/detail/ChannelDetailFragment.kt
@@ -5,23 +5,34 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.ChannelDetailFragmentBinding
+import com.example.heysrealprojcet.enums.ChannelForm
+import com.example.heysrealprojcet.enums.ChannelRecruitmentMethod
+import com.example.heysrealprojcet.enums.Gender
 import com.example.heysrealprojcet.model.network.NetworkResult
+import com.example.heysrealprojcet.ui.channel.list.detail.approvedUser.ApprovedUserImageListRecyclerViewAdapter
+import com.example.heysrealprojcet.ui.channel.list.detail.waitingUser.WaitingUserImageListRecyclerViewAdapter
 import com.example.heysrealprojcet.ui.main.MainActivity
 import com.example.heysrealprojcet.util.UserPreference
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ChannelDetailFragment : Fragment() {
+   private lateinit var mainActivity: MainActivity
    private lateinit var binding: ChannelDetailFragmentBinding
    private val viewModel by viewModels<ChannelDetailViewModel>()
-   var isExpanded = false
    val args: ChannelDetailFragmentArgs by navArgs()
+
+   private lateinit var approvedUserAdapter: ApprovedUserImageListRecyclerViewAdapter
+   private lateinit var waitingUserAdapter: WaitingUserImageListRecyclerViewAdapter
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -45,38 +56,55 @@ class ChannelDetailFragment : Fragment() {
       inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
    ): View? {
       binding = ChannelDetailFragmentBinding.inflate(inflater, container, false)
+      binding.vm = viewModel
       return binding.root
    }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
-      binding.vm = viewModel
 
       getChannelDetail(args.channelId)
-      binding.allMember.setOnClickListener { goToMemberList() }
-      binding.waitingHeysList.setOnClickListener { goToWaitingHeysList() }
-      binding.btnJoin.setOnClickListener { goToMyChannel() }
+      binding.allApprovedUser.setOnClickListener { goToApprovedUserList() }
+      binding.allWaitingUser.setOnClickListener { goToWaitingUserList() }
+
+      binding.btnJoin.setOnClickListener {  }
    }
 
-   private fun goToMemberList() {
-      findNavController().navigate(R.id.action_channelDetailFragment_to_memberHeysListFragment)
+   private fun goToApprovedUserList() {
+      viewModel.approvedUserList.observe(viewLifecycleOwner) {
+         it?.let {
+            findNavController().navigate(R.id.action_channelDetailFragment_to_approvedUserListFragment2, bundleOf("approvedUser" to it.toTypedArray()))
+         }
+      }
    }
 
-   private fun goToWaitingHeysList() {
-      findNavController().navigate(R.id.action_channelDetailFragment_to_waitingHeyListFragment)
-   }
-
-   private fun goToMyChannel() {
-      findNavController().navigate(R.id.action_channelDetailFragment_to_myChannelFragment)
+   private fun goToWaitingUserList() {
+      viewModel.waitingUserList.observe(viewLifecycleOwner) {
+         it?.let {
+            findNavController().navigate(R.id.action_channelDetailFragment_to_waitingHeyListFragment, bundleOf("waitingUser" to it.toTypedArray()))
+         }
+      }
    }
 
    private fun getChannelDetail(id: Int) {
       viewModel.getChannelDetail("Bearer ${UserPreference.accessToken}", id).observe(viewLifecycleOwner) { response ->
          when (response) {
             is NetworkResult.Success -> {
-               Log.i("getDetail: ", "success")
                viewModel.receiveChannelDetail(response.data?.channelDetail)
+               setChannelRegion()
+               setChannelRecruitmentMethod()
+               viewModel.channelForm.value?.let { setChannelForm(it) }
+               setLeaderImage()
+               setApprovedUserList()
+
+               // 승인 결정
+               if (viewModel.recruitMethod.value == ChannelRecruitmentMethod.Decide.methodEng) {
+                  setWaitingUserList()
+               } else {
+                  binding.waitingUserCount.visibility = View.GONE
+                  binding.waitingUserList.visibility = View.GONE
+               }
             }
 
             is NetworkResult.Error -> {
@@ -88,5 +116,110 @@ class ChannelDetailFragment : Fragment() {
             }
          }
       }
+   }
+
+   private fun setChannelRegion() {
+      //온라인이면 지역 텍스트 숨기기
+      if (viewModel.channelForm.value == ChannelForm.Online.engForm) {
+         binding.channelRegion.visibility = View.GONE
+      } else {
+         binding.channelRegion.visibility = View.VISIBLE
+      }
+   }
+
+   private fun setChannelRecruitmentMethod() {
+      binding.channelRecruitmentMethod.text = if (viewModel.recruitMethod.value == ChannelRecruitmentMethod.Approval.method) {
+         "승인없이 바로 참여가능해요."
+      } else {
+         "승인이 필요해요."
+      }
+   }
+
+   private fun setChannelForm(form: String) {
+      when (form) {
+         ChannelForm.Both.form -> {
+            binding.channelForm.text = ChannelForm.Both.form + "으로"
+         }
+         ChannelForm.Offline.form -> {
+            binding.channelForm.text = ChannelForm.Offline.form + "으로"
+         }
+         else -> {
+            binding.channelForm.text = ChannelForm.Online.form + "으로"
+         }
+      }
+   }
+
+   // 프로필 퍼센트에 따른 이미지 설정
+   private fun setLeaderImage() {
+      viewModel.leader.observe(viewLifecycleOwner) { leader ->
+         when (leader.percentage) {
+            in 0..49 -> {
+               when (UserPreference.gender) {
+                  Gender.Male.gender -> binding.leaderImage.setImageResource(R.drawable.ic_male_0)
+                  Gender.Female.gender -> binding.leaderImage.setImageResource(R.drawable.ic_female_0)
+                  else -> binding.leaderImage.setImageResource(R.drawable.ic_none_0)
+               }
+            }
+            in 50..99 -> {
+               when (UserPreference.gender) {
+                  Gender.Male.gender -> binding.leaderImage.setImageResource(R.drawable.ic_male_50)
+                  Gender.Female.gender -> binding.leaderImage.setImageResource(R.drawable.ic_female_50)
+                  else -> binding.leaderImage.setImageResource(R.drawable.ic_none_50)
+               }
+            }
+            100 -> {
+               when (UserPreference.gender) {
+                  Gender.Male.gender -> binding.leaderImage.setImageResource(R.drawable.ic_male_100)
+                  Gender.Female.gender -> binding.leaderImage.setImageResource(R.drawable.ic_female_100)
+                  else -> binding.leaderImage.setImageResource(R.drawable.ic_none_100)
+               }
+            }
+         }
+      }
+   }
+
+   private fun setApprovedUserList() {
+      viewModel.approvedUserList.value?.let {
+         if (it.size == 0) {
+            setApprovedUserListInvisible()
+         } else {
+            setApprovedUserListVisible()
+            approvedUserAdapter = ApprovedUserImageListRecyclerViewAdapter(it)
+            binding.approvedUserList.adapter = approvedUserAdapter
+            binding.approvedUserList.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+         }
+      }
+   }
+
+   private fun setApprovedUserListVisible() {
+      binding.approvedUserListEmpty.visibility = View.GONE
+      binding.approvedUserList.visibility = View.VISIBLE
+   }
+
+   private fun setApprovedUserListInvisible() {
+      binding.approvedUserListEmpty.visibility = View.VISIBLE
+      binding.approvedUserList.visibility = View.GONE
+   }
+
+   private fun setWaitingUserList() {
+      viewModel.waitingUserList.value?.let {
+         if (it.size == 0) {
+            setWaitingUserListInvisible()
+         } else {
+            setWaitingUserListVisible()
+            binding.waitingUserList.adapter = waitingUserAdapter
+            binding.waitingUserList.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+         }
+      }
+   }
+
+   private fun setWaitingUserListVisible() {
+      binding.waitingUserListEmpty.visibility = View.GONE
+      binding.waitingUserList.visibility = View.VISIBLE
+   }
+
+   private fun setWaitingUserListInvisible() {
+      binding.waitingUserListEmpty.visibility = View.VISIBLE
+      binding.waitingUserList.visibility = View.GONE
    }
 }
--- app/src/main/java/com/example/heysrealprojcet/ui/channel/list/detail/ChannelDetailViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/channel/list/detail/ChannelDetailViewModel.kt
@@ -7,6 +7,7 @@ import com.example.heysrealprojcet.model.*
 import com.example.heysrealprojcet.repository.ChannelRepository
 import com.example.heysrealprojcet.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -45,8 +46,8 @@ class ChannelDetailViewModel @Inject constructor(
    private val _leader = MutableLiveData<Leader>()
    val leader: LiveData<Leader> = _leader
 
-   private val _channelPurposes = MutableLiveData<ArrayList<ChannelPurpose>>()
-   val channelPurposes: LiveData<ArrayList<ChannelPurpose>> = _channelPurposes
+   private val _purposes = MutableLiveData<ArrayList<ChannelPurpose>>()
+   val purposes: LiveData<ArrayList<ChannelPurpose>> = _purposes
 
    private val _links = MutableLiveData<ArrayList<ChannelLink>>()
    val links: LiveData<ArrayList<ChannelLink>> = _links
@@ -72,8 +73,14 @@ class ChannelDetailViewModel @Inject constructor(
    private val _recruitEndTime = MutableLiveData<String>()
    val recruitEndTime: LiveData<String> = _recruitEndTime
 
-   private val _channelInterest = MutableLiveData<String>()
-   val channelInterest: LiveData<String> = _channelInterest
+   private val _interestString by lazy { MutableLiveData("") }
+   val interestString: LiveData<String> = _interestString
+
+   private val _purposeString = MutableLiveData("")
+   val purposeString: LiveData<String> = _purposeString
+
+   private val _waitingUserNum = MutableLiveData<Int>()
+   val waitingUserNum: LiveData<Int> = _waitingUserNum
 
    fun getChannelDetail(token: String, id: Int) = channelRepository.getChannelDetail(token, id).asLiveData()
 
@@ -85,17 +92,43 @@ class ChannelDetailViewModel @Inject constructor(
          _location.value = it.location
          _capacity.value = it.limit
          _recruitMethod.value = it.recruitMethod
-         _recruitEndDate.value = it.lastRecruitDate
+         _recruitEndDateTime.value = it.lastRecruitDate
          _interests.value = it.interests
          _contentText.value = it.contentText
+         _recruitText.value = it.recruitText
          _leader.value = it.leader
-         _channelPurposes.value = it.purposes
+         _purposes.value = it.purposes
          _links.value = it.links
          _contentData.value = it.contentData
          _relationshipWithMe.value = it.relationshipWithMe
          _isBookMarked.value = it.isBookMarked
          _approvedUserList.value = it.approvedUserList
          _waitingUserList.value = it.waitingUserList
+         _waitingUserNum.value = it.waitingUserList.size
       }
+      setEndDateTimeString()
+      setPurposeString()
+      setInterestString()
+   }
+
+   private fun setEndDateTimeString() {
+      var datetime = LocalDateTime.parse(recruitEndDateTime.value)
+      var year = datetime.year.minus(2000)
+      var month = datetime.monthValue
+      var day = datetime.dayOfMonth
+      var hour = datetime.hour
+      var min = datetime.minute
+      _recruitEndDate.value = "${year}년 ${month}월 ${day}일"
+      _recruitEndTime.value = "${hour}시 ${min}분"
+   }
+
+   private fun setPurposeString() {
+      purposes.value?.forEach { _purposeString.value += "${it.purpose}, " }
+      _purposeString.value = _purposeString.value?.dropLast(2)
+   }
+
+   private fun setInterestString() {
+      interests.value?.forEach { _interestString.value += "${it}, " }
+      _interestString.value = _interestString.value?.dropLast(2)
    }
 }
