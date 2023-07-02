5b35b899f43599a48b02f764f250d62ec4d730b1
Jiwon <senta2006@naver.com>
2023-07-02 22:50:35 +0900

    [Feat] 로그아웃 시 device token 삭제 api 연동

--- app/src/main/java/com/example/heysrealprojcet/ui/user/setting/SettingFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/setting/SettingFragment.kt
@@ -2,20 +2,25 @@ package com.example.heysrealprojcet.ui.user.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.SettingFragmentBinding
+import com.example.heysrealprojcet.model.network.NetworkResult
 import com.example.heysrealprojcet.ui.intro.IntroActivity
 import com.example.heysrealprojcet.ui.main.MainActivity
 import com.example.heysrealprojcet.util.UserPreference
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingFragment : Fragment() {
    private lateinit var binding: SettingFragmentBinding
-
+   private val viewModel: SettingViewModel by viewModels()
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
    ): View? {
@@ -48,7 +53,7 @@ class SettingFragment : Fragment() {
       binding.phoneChange.setOnClickListener { gotoPhoneChange() }
       binding.passwordChange.setOnClickListener { gotoPasswordVerification() }
       binding.deleteAccount.setOnClickListener { gotoDeleteAccount() }
-      binding.tvLogout.setOnClickListener { goToIntro() }
+      binding.tvLogout.setOnClickListener { logout() }
    }
 
    private fun gotoDeleteAccount() {
@@ -63,10 +68,30 @@ class SettingFragment : Fragment() {
       findNavController().navigate(R.id.action_settingFragment_to_settingPasswordVerificationFragment)
    }
 
-   private fun goToIntro() {
-      UserPreference.isAutoLogin = false
+   private fun logout() {
+      deleteDeviceToken()
+
+      UserPreference.init()
       val intent = Intent(requireActivity(), IntroActivity::class.java)
       startActivity(intent)
       requireActivity().finish()
+   }
+
+   private fun deleteDeviceToken() {
+      viewModel.deleteDeviceToken("Bearer ${UserPreference.accessToken}", UserPreference.deviceToken).observe(viewLifecycleOwner) { response ->
+         when (response) {
+            is NetworkResult.Success -> {
+               Log.d("deleteDeviceToken: ", response.data?.message.toString())
+            }
+
+            is NetworkResult.Error -> {
+               Log.w("deleteDeviceToken: ", "error ${response.message}")
+            }
+
+            is NetworkResult.Loading -> {
+               Log.i("deleteDeviceToken: ", "loading")
+            }
+         }
+      }
    }
 }
--- app/src/main/java/com/example/heysrealprojcet/ui/user/setting/SettingViewModel.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/user/setting/SettingViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.heysrealprojcet.ui.user.setting
 
-class SettingViewModel {
+import androidx.lifecycle.asLiveData
+import com.example.heysrealprojcet.repository.MyPageRepository
+import com.example.heysrealprojcet.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(private val myPageRepository: MyPageRepository) : BaseViewModel() {
+   fun deleteDeviceToken(token: String, deviceToken: String) =
+      myPageRepository.deleteDeviceToken(token, deviceToken).asLiveData()
 }
