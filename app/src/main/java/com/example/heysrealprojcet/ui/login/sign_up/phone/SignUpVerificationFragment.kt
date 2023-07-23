3f20c6b7b5827c27d65700326937611ac0a82c82
Ddudduu <50603005+Ddudduu@users.noreply.github.com>
2023-07-24 01:08:05 +0900

    휴대폰 인증 API 연동 (#3)
    * [Feat] 휴대폰 인증번호 전송 api 연동
    
    * [Feat] PhoneVerification 데이터 클래스 추가
    
    * [Feat] 휴대폰 인증 검증 api 연동
    
    * [Fix] 휴대폰 인증 검증 parameter 수정

--- app/src/main/java/com/example/heysrealprojcet/ui/login/sign_up/phone/SignUpVerificationFragment.kt
+++ app/src/main/java/com/example/heysrealprojcet/ui/login/sign_up/phone/SignUpVerificationFragment.kt
@@ -2,45 +2,30 @@ package com.example.heysrealprojcet.ui.login.sign_up.phone
 
 import android.content.Context
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
+import com.example.heysrealprojcet.CustomSnackBar
 import com.example.heysrealprojcet.R
 import com.example.heysrealprojcet.databinding.SignUpPhoneVerificationFragmentBinding
+import com.example.heysrealprojcet.model.network.NetworkResult
+import com.example.heysrealprojcet.model.network.Phone
+import com.example.heysrealprojcet.model.network.PhoneVerification
 import com.example.heysrealprojcet.ui.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpVerificationFragment : Fragment() {
    private lateinit var binding: SignUpPhoneVerificationFragmentBinding
    private val viewModel: SignUpVerificationViewModel by viewModels()
-
-   //   private lateinit var firebaseAuth: FirebaseAuth
-   private var storedVerificationId = ""
-//   private var resendToken: PhoneAuthProvider.ForceResendingToken? = null
-
-//   private val callbacks by lazy {
-//      object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//         override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-//            Toast.makeText(requireContext(), "인증코드가 전송되었습니다. 90초 이내에 입력해주세요", Toast.LENGTH_LONG).show()
-//            verifyPhoneNumberWithCode(credential)
-//         }
-//
-//         @SuppressLint("LongLogTag")
-//         override fun onVerificationFailed(e: FirebaseException) {
-//            Log.w("=== onVerificationFailed ===", e)
-//            Toast.makeText(requireContext(), "휴대폰 인증에 실패했습니다.", Toast.LENGTH_LONG).show()
-//         }
-//
-//         override fun onCodeSent(verficationId: String, token: PhoneAuthProvider.ForceResendingToken) {
-//            Log.d("=== onCodeSent ===", verficationId)
-//            storedVerificationId = verficationId
-//            resendToken = token
-//         }
-//      }
-//   }
+   private lateinit var countDownTimer: CountDownTimer
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -65,102 +50,105 @@ class SignUpVerificationFragment : Fragment() {
    ): View? {
       binding = SignUpPhoneVerificationFragmentBinding.inflate(inflater, container, false)
       binding.vm = viewModel
-//      firebaseAuth = Firebase.auth
-      binding.okButton.isEnabled = true
-      //initViewModelCallback()
       return binding.root
    }
-
-//   @SuppressLint("LongLogTag")
-//   private fun initViewModelCallback() {
-//      with(viewModel) {
-//         requestPhoneAuth.observe(viewLifecycleOwner) {
-//            if (it) {
-//               viewModel.phoneAuthNumber = ""
-//               val phoneNumber = "+82" + UserPreference.phoneNumber.substring(1)
-//               startPhoneNumberVerification(phoneNumber)
-//               Log.w("=== phoneNum ===", phoneNumber)
-//            } else {
-//               Toast.makeText(requireContext(), "전화번호를 입력해주세요", Toast.LENGTH_LONG).show()
-//            }
-//         }
-//
-//         requestResendPhoneAuth.observe(viewLifecycleOwner) {
-//            if (it) {
-//               viewModel.phoneAuthNumber = ""
-//               val phoneNumber = "+82" + UserPreference.phoneNumber.substring(1)
-//               resendVerificationCode(phoneNumber, resendToken)
-//            }
-//         }
-//
-//         is6Digit.observe(viewLifecycleOwner) {
-//            if (it) {
-//               Log.w("=== StoredVerification ID ===", storedVerificationId)
-//               Log.w("=== Verification Number ===", viewModel.verificationNumber.value)
-//               val phoneCredential = PhoneAuthProvider.getCredential(storedVerificationId, viewModel.verificationNumber.value)
-//               verifyPhoneNumberWithCode(phoneCredential)
-//            } else {
-//               binding.okButton.isEnabled = false
-//            }
-//         }
-//      }
-//   }
-
-//   private fun startPhoneNumberVerification(phoneNumber: String) {
-//      val options = PhoneAuthOptions.newBuilder(firebaseAuth)
-//         .setPhoneNumber(phoneNumber)
-//         .setTimeout(120L, TimeUnit.SECONDS)
-//         .setActivity(requireActivity())
-//         .setCallbacks(callbacks)
-//         .build()
-//      PhoneAuthProvider.verifyPhoneNumber(options)
-//   }
-
-//   private fun resendVerificationCode(phoneNumber: String, token: PhoneAuthProvider.ForceResendingToken?) {
-//      val optionsBuilder = PhoneAuthOptions.newBuilder(firebaseAuth)
-//         .setPhoneNumber(phoneNumber)
-//         .setTimeout(120L, TimeUnit.SECONDS)
-//         .setActivity(requireActivity())
-//         .setCallbacks(callbacks)
-//
-//      if (token != null) {
-//         optionsBuilder.setForceResendingToken(token)
-//      }
-//      PhoneAuthProvider.verifyPhoneNumber(optionsBuilder.build())
-//   }
-
-//   @SuppressLint("LongLogTag")
-//   private fun verifyPhoneNumberWithCode(phoneAuthCredential: PhoneAuthCredential) {
-//      Firebase.auth.signInWithCredential(phoneAuthCredential)
-//         .addOnCompleteListener(requireActivity()) { task ->
-//            if (task.isSuccessful) {
-//               Log.d("=== signInWithCredential ===", "success")
-//               binding.okButton.isEnabled = true
-//            } else {
-//               Log.w("=== verify Error ===", task.exception.toString())
-//               Toast.makeText(requireContext(), "인증 번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
-//            }
-//         }
-//   }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
 
       viewModel.phoneNumber.value = arguments?.getString("phoneNumber")
-      if (viewModel.phoneNumber.value!!.isNotEmpty()) {
-         viewModel.timerStart()
-      }
-      binding.okButton.setOnClickListener { goToJoinPassword() }
+      setKeyboard()
 
-      // 화면 들어오자마자 키보드 보이기
-      val inputMethodManager =
-         requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-      binding.phoneInput.requestFocus()
-      inputMethodManager.showSoftInput(binding.phoneInput, 0)
+      countDownTimer = startTimer(3 * 60 * 1000L)
+      countDownTimer?.start()
+
+      viewModel.phoneNumber.observe(viewLifecycleOwner) { phone ->
+         phone?.let { postPhoneVerification(phone) }
+
+         viewModel.code.asLiveData().observe(viewLifecycleOwner) { code ->
+            binding.btnOk.setOnClickListener { code?.let { deletePhoneVerification(code, phone) } }
+         }
+      }
+
+
+      binding.btnResned.setOnClickListener {
+         countDownTimer?.cancel()
+         countDownTimer = startTimer(3 * 60 * 1000L)
+         countDownTimer?.start()
+
+         viewModel.phoneNumber.observe(viewLifecycleOwner) { it?.let { postPhoneVerification(it) } }
+      }
    }
 
    private fun goToJoinPassword() {
       findNavController().navigate(R.id.action_signUpVerificationFragment_to_signUpPasswordFragment)
+   }
+
+   private fun setKeyboard() {
+      // 화면 들어오자마자 키보드 보이기
+      val inputMethodManager =
+         requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      binding.etCode.requestFocus()
+      inputMethodManager.showSoftInput(binding.etCode, 0)
+   }
+
+   private fun postPhoneVerification(phoneNumber: String) {
+      viewModel.postPhoneVerification(Phone(phone = phoneNumber.replace("-", ""))).observe(viewLifecycleOwner) { response ->
+         when (response) {
+            is NetworkResult.Success -> {
+               Log.d("postPhoneVerification: ", response.data?.message.toString())
+            }
+
+            is NetworkResult.Error -> {
+               Log.w("postPhoneVerification: ", "error ${response.message}")
+            }
+
+            is NetworkResult.Loading -> {
+               Log.i("postPhoneVerification: ", "loading")
+            }
+         }
+      }
+   }
+
+   private fun deletePhoneVerification(code: String, phoneNumber: String) {
+      val phoneVerification = PhoneVerification(code, phoneNumber.replace("-", ""))
+
+      viewModel.deletePhoneVerification(phoneVerification).observe(viewLifecycleOwner) { response ->
+         when (response) {
+            is NetworkResult.Success -> {
+               Log.d("deletePhoneVeri: ", response.data?.message.toString())
+               if (response.data?.data?.isVerification == true) {
+                  goToJoinPassword()
+               } else {
+                  CustomSnackBar(binding.root, "인증번호가 일치하지 않아요! \n SMS 문자를 확인해주세요.", binding.btnOk).show()
+               }
+            }
+
+            is NetworkResult.Error -> {
+               Log.w("deletePhoneVeri: ", "error ${response.message}")
+            }
+
+            is NetworkResult.Loading -> {
+               Log.i("deletePhoneVeri: ", "loading")
+            }
+         }
+      }
+   }
+
+   private fun startTimer(initialMillis: Long): CountDownTimer = object : CountDownTimer(initialMillis, 1000L) {
+      override fun onTick(millisUntilFinished: Long) {
+         updateTime(millisUntilFinished)
+      }
+
+      override fun onFinish() {
+
+      }
+   }
+
+   private fun updateTime(remainMillis: Long) {
+      var remainSeconds = remainMillis / 1000
+      binding.tvMinute.text = "%02d".format(remainSeconds / 60)
+      binding.tvSecond.text = "%02d".format(remainSeconds % 60)
    }
 }
