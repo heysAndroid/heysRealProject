0c9046fb71538d0237eba19da9962f4f77703e96
Jiwon <imjiwon@imjiwon-ui-noteubug.local>
2024-02-29 20:50:17 +0900

    [Fix] 회원가입 생년월일 선택 방식 수정

--- app/src/main/java/com/hey/heys/ui/login/sign_up/age/SignUpAgeFragment.kt
+++ app/src/main/java/com/hey/heys/ui/login/sign_up/age/SignUpAgeFragment.kt
@@ -2,9 +2,11 @@ package com.hey.heys.ui.login.sign_up.age
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -47,24 +49,43 @@ class SignUpAgeFragment : Fragment() {
       super.onViewCreated(view, savedInstanceState)
       binding.lifecycleOwner = this
       binding.btnBack.setOnClickListener { findNavController().navigateUp() }
-      binding.okButton.setOnClickListener { goToInterest() }
-      binding.datePicker.maxDate = Calendar.getInstance().timeInMillis
-      binding.datePicker.setOnDateChangedListener { _, year, month, dayOfMonth ->
-         enableOkButton()
-         UserPreference.birthday = "$year-${(month + 1).toString().convertSingleToDoubleDigit()}-${dayOfMonth.toString().convertSingleToDoubleDigit()}"
-         val birthday = LocalDate.of(year, month + 1, dayOfMonth)
-         UserPreference.age = getInternationalAge(birthday)
+
+      binding.okButton.setOnClickListener {
+         UserPreference.birthday = "${binding.npYear.value}-${binding.npMonth.value.toString().convertSingleToDoubleDigit()}-${binding.npDate.value.toString().convertSingleToDoubleDigit()}"
+         UserPreference.age = getInternationalAge(binding.npYear.value)
+         goToInterest()
+      }
+
+      binding.npYear.apply {
+         wrapSelectorWheel = false
+         descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+         minValue = 1
+         maxValue = Calendar.getInstance().get(Calendar.YEAR)
+         value = Calendar.getInstance().get(Calendar.YEAR)
+      }
+
+      binding.npMonth.apply {
+         wrapSelectorWheel = false
+         descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+         minValue = 1
+         maxValue = 12
+         value = Calendar.getInstance().get(Calendar.MONTH)
+      }
+
+      binding.npDate.apply {
+         wrapSelectorWheel = false
+         descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+         minValue = 1
+         maxValue = 31
+         value = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
       }
    }
 
-   private fun enableOkButton() {
-      binding.okButton.isEnabled = true
-   }
+   private fun getInternationalAge(year: Int): Int {
+      val curYear = LocalDate.now().year
+      var internationalAge = curYear - year
 
-   private fun getInternationalAge(birthday: LocalDate): Int {
-      val now = LocalDate.now()
-      var internationalAge = now.minusYears(birthday.year.toLong()).year
-      if (birthday.plusYears(internationalAge.toLong()).isAfter(now)) {
+      if (year + internationalAge > curYear) {
          internationalAge -= 1
       }
 
